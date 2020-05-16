c9be9e2c66fa17a14037c412fefc17c0f6617c33
Gaylord Poillon <gaylord.p@gmail.com>
2020-05-16 11:05:43 +0200

    Add JS

---
+++ Resources/public/front_common.js
@@ -0,0 +1,152 @@
+import $ from 'jquery'
+
+class FrontCommon
+{
+    constructor()
+    {
+        this.$modal = null
+        this.modalIsOpen = false
+        let self = this
+
+        $('body').on('click', 'a[data-front-common-ajax]', function(e) {
+            e.preventDefault()
+
+            self.ajaxLink($(this))
+        })
+
+        $('body').on('submit', 'form[data-form-ajax]', function(e) {
+            e.preventDefault()
+
+            self.ajaxFormSubmit($(this))
+        })
+    }
+
+    ajaxLink($element) {
+        let self = this
+
+        $.ajax({
+            url: $element.attr('href'),
+            dataType: 'json',
+            complete: function(xhr) {
+                let json = xhr.responseJSON
+
+                if (false === Array.isArray(json)) {
+                    self.ajaxJsonResponse(json)
+                } else {
+                    for (let i = 0; i < json.length; i++) {
+                        self.ajaxJsonResponse(json[i])
+                    }
+                }
+            },
+        })
+    }
+
+    ajaxFormSubmit($form) {
+        let self = this
+
+        $.ajax({
+            url: $form.attr('action'),
+            method: 'POST',
+            data: $form.serialize(),
+            dataType: 'json',
+            complete: function(xhr) {
+                $form[0].reset()
+                let json = xhr.responseJSON
+
+                if (false === Array.isArray(json)) {
+                    self.ajaxJsonResponse(json)
+                } else {
+                    for (let i = 0; i < json.length; i++) {
+                        self.ajaxJsonResponse(json[i])
+                    }
+                }
+            },
+        })
+    }
+
+    ajaxJsonResponse(json)
+    {
+        let self = this
+        let $target = $(json.target)
+
+        if ('after' === json.action || 'append' === json.action || 'before' === json.action || 'prepend' === json.action) {
+            let $html = $(json.html)
+            let $layer = $('<div class="bg-success" style="width: 100%; height: 100%; position: absolute; opacity: 0.25"></div>')
+            $html.append($layer)
+
+            switch (json.action) {
+                case 'after':
+                    $target.after($html)
+                break;
+                case 'append':
+                    $target.append($html)
+                break;
+                case 'before':
+                    $target.before($html)
+                break;
+                case 'prepend':
+                    $target.prepend($html)
+                break;
+            }
+
+            $html.find('[data-toggle="tooltip"]').tooltip()
+
+            if (true === self.modalIsOpen) {
+                self.$modal.animate({
+                    scrollTop: self.$modal.find('.modal-dialog').height()
+                }, 1000)
+            }
+
+            setTimeout(function () {
+                $layer.fadeOut()
+            }, 5000)
+        } else if ('html' === json.action) {
+            $target.html(json.html)
+        } else if ('replace' === json.action) {
+            let $html = $(json.html)
+
+            $target.replaceWith($html)
+
+            $html.find('[data-toggle="tooltip"]').tooltip()
+        } else if ('show-modal' === json.action) {
+            self.modal(json.title, json.body)
+        }
+    }
+
+    modal(title, body) {
+        let self = this
+
+        if (null === this.$modal) {
+            this.$modal = $('<div class="modal fade" id="frontCommonModal" tabindex="-1" role="dialog" aria-labelledby="frontCommonModalTitle" aria-hidden="true">\n' +
+            '    <div class="modal-dialog modal-dialog-centered modal-xl" role="document">\n' +
+            '        <div class="modal-content">\n' +
+            '            <div class="modal-header">\n' +
+            '                <h5 class="modal-title"></h5>\n' +
+            '                <button type="button" class="close" data-dismiss="modal" aria-label="Close">\n' +
+            '                    <i class="fas fa-times"></i>\n' +
+            '                </button>\n' +
+            '            </div>\n' +
+            '            <div class="modal-body"></div>\n' +
+            '            </div>\n' +
+            '        </div>\n' +
+            '    </div>\n')
+
+            $('body').prepend(this.$modal)
+
+            this.$modal.on('show.bs.modal', function (e) {
+                self.modalIsOpen = true
+            })
+
+            this.$modal.on('hidden.bs.modal', function (e) {
+                self.modalIsOpen = false
+            })
+        }
+
+        this.$modal.find('.modal-title').html(title);
+        this.$modal.find('.modal-body').html(body);
+        this.$modal.modal('show')
+        this.$modal.find('[data-toggle="tooltip"]').tooltip()
+    }
+}
+
+new FrontCommon()
